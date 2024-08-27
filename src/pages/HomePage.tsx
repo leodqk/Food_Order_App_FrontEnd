d731be1fc0eca0d3441d18498d97fdc83a078286
Dương Quang Khải <94659049+leodqk@users.noreply.github.com>
2024-08-27 16:28:39 +0700

    add_searchBar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,17 @@
-import landing from "../assets/landing.png";
-import appDownload from "../assets/appDownload.png";
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
       <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
@@ -8,9 +19,13 @@ const HomePage = () => {
           Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
-        <img src={landing} alt="" />
+        <img src={landingImage} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
             Order takeaway even faster!
@@ -19,7 +34,7 @@ const HomePage = () => {
             Download the MernEats App for faster ordering and personalised
             recommendations
           </span>
-          <img src={appDownload} alt="" />
+          <img src={appDownloadImage} />
         </div>
       </div>
     </div>
